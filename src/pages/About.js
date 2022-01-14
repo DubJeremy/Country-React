9ea88559b8eb479ae68ef31a46e6997f56c5e259
DubJ <jeremy.ah33@gmail.com>
2022-01-14 11:32:39 +0100

    init

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h2>A propos</h2>
+            <br />
+            <p>
+                Dolore aliquip labore tempor tempor adipisicing irure voluptate nisi consequat Lorem dolore in. Magna id id dolore dolor in pariatur sunt. Minim quis duis in ad qui ad dolore id qui excepteur in excepteur. Proident excepteur sit adipisicing aliqua.
+            </p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Navigation />
+            <h2>Acceuil</h2>
+        </div>
+    )
+}
+
+export default Home;
