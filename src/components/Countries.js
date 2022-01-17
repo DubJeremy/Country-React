ad3c73ba530180c2f39cb77660cf56b9d6652183
DubJ <jeremy.ah33@gmail.com>
2022-01-17 15:15:13 +0100

    API countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
 const Countries = () => {
     const [data, setData] = useState([]);
 
-    axios.get(
-        'https://restcountries.com/v3.1/all?fields=name;population;region;capital;flag'
-        )
-        .then((res) => console.log(res))
+    useEffect(() => {
+        axios
+            .get(
+                'https://restcountries.com/v3.1/all'
+            )
+            .then((res) => setData(res.data));
+
+            console.log(data);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
-        <div>
+        <div className='countries'>
+            <ul className='countries-list'>
+                {data.map((country) => (
+                    <li>
+                        {country.name}
+                    </li>
+                ))}
+            </ul>
 
         </div>
     );
